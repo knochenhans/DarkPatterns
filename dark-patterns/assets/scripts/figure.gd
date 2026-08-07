f637a19ac7b21ac17381662bcee4908ad0a73e65
bruttosozial <jonathanvonwerder@posteo.de>
2026-08-07 23:05:58 +0200

    figure spawning, random movement

--- dark-patterns/assets/scripts/figure.gd
+++ dark-patterns/assets/scripts/figure.gd
@@ -1,24 +1,41 @@
-extends Node2D
+extends CharacterBody2D
 
 var happyness = 100
-
 var affected = true
+var play_area = null
+
+
+var move_counter = 0
+var move_target: Vector2 = Vector2.ZERO
 
 func _ready():
 	pass
-	
-func _process(delta: float) -> void:
+
+func _process(_delta) -> void:
 	if happyness > 50:
 		$AnimatedSprite2D.animation = "smile"
 	else:
 		$AnimatedSprite2D.animation = "sad"
-	pass
 
+
+func _physics_process(_delta: float) -> void:
+	if move_target != Vector2.ZERO:
+		var move_vector: Vector2 = move_target - position
+		velocity = move_vector.normalized() * 100
+		move_and_slide()
+
+func get_random_move_location():
+	var bounds = play_area.shape.size
+	return Vector2(randf_range(0, bounds.x), randf_range(0, bounds.y))
 
 func _on_tick_timeout() -> void:
+	if move_counter == 5:
+		move_target = get_random_move_location()
+		move_counter = 0
+	else:
+		move_counter += 1
 	if affected:
-		happyness = happyness - 2
-		
+		happyness -= 2
 		if happyness > 100:
 			happyness = 100
 		if happyness < 0:
--- dark-patterns/assets/scripts/main.gd
+++ dark-patterns/assets/scripts/main.gd
@@ -2,6 +2,7 @@ extends Node
 
 var ingame = true
 
+var figure = preload("res://figure.tscn")
 func _ready() -> void:
 	pass
 
@@ -16,4 +17,13 @@ func _on_tick_timeout() -> void:
 		pass
 
 func spawn_figure():
+	
+	var figure_instance = figure.instantiate()
+	figure_instance.play_area = $play_area
+	figure_instance.position = get_random_spawn_location()
+	$figures.add_child(figure_instance)
 	pass
+
+func get_random_spawn_location():
+	var bounds = $play_area.shape.size
+	return Vector2(randf_range(0, bounds.x), randf_range(0, bounds.y))

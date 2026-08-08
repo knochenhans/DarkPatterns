97d96bfadac964b30df6ef40bbb935d87ea647ca
Andre Jonas <nipsky@googlemail.com>
2026-08-08 17:07:17 +0200

    Working on patterns

--- dark-patterns/assets/scenes/pattern_button.gd
+++ dark-patterns/assets/scenes/pattern_button.gd
@@ -3,6 +3,8 @@ class_name PatternButton
 
 signal pattern_selected(pattern: DarkPattern)
 
+@export var pattern_icon: TextureRect
+
 var pattern: DarkPattern
 
 func _ready() -> void:
@@ -12,6 +14,7 @@ func set_pattern(p: DarkPattern) -> void:
     pattern = p
     # self.texture_normal = pattern.icon
     self.modulate = pattern.color
+    self.pattern_icon.texture = pattern.icon
 
 func _on_pressed() -> void:
     emit_signal("pattern_selected", pattern)

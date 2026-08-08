4d59fb843cf71e0301ef6ed5e3643153c620c231
bruttosozial <jonathanvonwerder@posteo.de>
2026-08-08 20:52:31 +0200

    re-orient money counter

--- dark-patterns/assets/scenes/money_container.gd
+++ dark-patterns/assets/scenes/money_container.gd
@@ -3,4 +3,4 @@ extends PanelContainer
 @export var money_label: Label
 
 func _process(_delta: float) -> void:
-    money_label.text = str(Global.money, "") + "$"
+	money_label.text = str(Global.money, "") + "$"

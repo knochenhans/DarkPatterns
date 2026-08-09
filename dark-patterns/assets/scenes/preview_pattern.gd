46a59a8265e5aad7755709a5fcc89a9cfe13d468
Andre Jonas <nipsky@googlemail.com>
2026-08-09 10:46:43 +0200

    Enhance pattern preview by adding icon sprite and shader material

--- dark-patterns/assets/scenes/preview_pattern.gd
+++ dark-patterns/assets/scenes/preview_pattern.gd
@@ -4,9 +4,12 @@ class_name PatternPreview
 var dark_pattern: DarkPattern
 
 @export var polygon_2d: Polygon2D
+@export var icon_sprite: Sprite2D
 
 func _ready() -> void:
     set_polygon_2d()
+    icon_sprite.texture = dark_pattern.icon
+    icon_sprite.scale = Vector2(dark_pattern.size/75, dark_pattern.size/75)
 
 func set_polygon_2d():
     PatternHelper.set_polygon_2d(polygon_2d, dark_pattern)

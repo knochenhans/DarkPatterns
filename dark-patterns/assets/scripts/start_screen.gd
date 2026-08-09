33487dfc4823ea5178d16d33a48d0b1c277aa06e
Andre Jonas <nipsky@googlemail.com>
2026-08-09 14:03:42 +0200

    Add machine start sound effect as an Ogg Vorbis audio stream
    - Imported machine_start.ogg as an audio resource.
    - Configured audio parameters including looping and beat settings.

--- dark-patterns/assets/scripts/start_screen.gd
+++ dark-patterns/assets/scripts/start_screen.gd
@@ -2,6 +2,7 @@ extends Control
 class_name StartScreen
 
 @export var start_button: TextureButton
+@export var audio_stream_player: AudioStreamPlayer
 
 signal start_game_requested()
 
@@ -9,4 +10,5 @@ func _ready() -> void:
 	start_button.connect("pressed", Callable(self, "_on_start_button_pressed"))
 
 func _on_start_button_pressed() -> void:
+	audio_stream_player.play()
 	emit_signal("start_game_requested")

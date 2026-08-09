9e0fb93901601f28c348bfa30e090cdd2040dfdd
bruttosozial <jonathanvonwerder@posteo.de>
2026-08-09 10:03:25 +0200

    humanize money counter

--- dark-patterns/assets/scenes/money_container.gd
+++ dark-patterns/assets/scenes/money_container.gd
@@ -3,4 +3,24 @@ extends Control
 @export var money_label: Label
 
 func _process(_delta: float) -> void:
-	money_label.text = str(Global.money, "") + "$"
+	money_label.text = humanize_number(str(Global.money, "")) + "$"
+
+
+# gratefully taken from
+# https://forum.godotengine.org/t/humanize-numbers/88273/3
+func humanize_number(number : String) -> String:
+	var to_return : String
+	var decimals : String
+	if "." in number:
+		decimals = "." + number.split(".", false, 0)[1]
+	if len(number.replace(decimals, "")) < 4:
+		return number
+	else:
+		var i : int = 0
+		for item in number.replace(decimals, "").reverse():
+			if i == 3:
+				item += "."
+				i = 0
+			to_return = item + to_return
+			i += 1
+		return to_return + decimals

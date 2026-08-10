1b5923fe7465bd1474b5ee8f77dc89584145c12e
Tim <MrArkandor@users.noreply.github.com>
2026-08-10 19:05:04 +0200

    change settinge to fit mockup

--- dark-patterns/assets/scripts/ui_settings.gd
+++ dark-patterns/assets/scripts/ui_settings.gd
@@ -1,17 +1,22 @@
 extends Control
-@export var fullscreen_btn: Button
-@export var volume: VSlider
+@export var fullscreen_btn: BaseButton
+@export var volume: Slider
+@export var fullscreen_light_on: TextureRect
 
 func _ready():
-	fullscreen_btn.toggled.connect(fullscreen_toggled)
+	fullscreen_btn.pressed.connect(toggle_fullscreen)
 	volume.value_changed.connect(volume_changed)
 	volume.value = AudioServer.get_bus_volume_linear(0)
 	fullscreen_btn.button_pressed = true if DisplayServer.window_get_mode(0) == DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN else false
-
-func fullscreen_toggled(toggled_on: bool) -> void:
-	var mode := DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN if toggled_on else DisplayServer.WindowMode.WINDOW_MODE_WINDOWED
+	
+func toggle_fullscreen() -> void:
+	var mode := DisplayServer.WindowMode.WINDOW_MODE_WINDOWED if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN else DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN
 	DisplayServer.window_set_mode(mode)
-
+	change_light()
+	
+func change_light():
+	fullscreen_light_on.visible = true if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN else false
+	
 
 func volume_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(0,value)

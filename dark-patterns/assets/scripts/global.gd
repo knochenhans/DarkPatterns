3ea43b6582bd38d0d5c50b57339a9efd0aef2c81
Tim <MrArkandor@users.noreply.github.com>
2026-08-10 22:09:45 +0200

    cursor fix electric boogaloo

--- dark-patterns/assets/scripts/global.gd
+++ dark-patterns/assets/scripts/global.gd
@@ -10,18 +10,25 @@ enum cursors {normal,no_money,bad_placement}
 var current_cursor : cursors
 var cursor_override : bool = false
 func change_mouse_cursor(cursor : cursors):
-	if cursor_override and current_cursor != cursor:
-		current_cursor = cursor
-		match cursor:
-			cursors.bad_placement:
-				Input.set_custom_mouse_cursor(cursor_bad_place,Input.CursorShape.CURSOR_ARROW,cursor_custom_offset)
-			cursors.no_money:
-				Input.set_custom_mouse_cursor(cursor_no_money,Input.CursorShape.CURSOR_ARROW,cursor_custom_offset)
-			_:
-				Input.set_custom_mouse_cursor(null,Input.CursorShape.CURSOR_ARROW)
+	if cursor_override:
+		if current_cursor != cursor:
+			current_cursor = cursor
+			set_cursor(cursor)
 
 func change_cursor_override(override : bool):
+	if cursor_override == override:
+		return
 	cursor_override = override
 	if override == false:
-		#print("c:override:",override)
-		change_mouse_cursor(Global.cursors.normal)
+		set_cursor(cursors.normal)
+	else:
+		set_cursor(current_cursor)
+	
+func set_cursor(cursor : cursors):
+	match cursor:
+		cursors.bad_placement:
+			Input.set_custom_mouse_cursor(cursor_bad_place,Input.CursorShape.CURSOR_ARROW,cursor_custom_offset)
+		cursors.no_money:
+			Input.set_custom_mouse_cursor(cursor_no_money,Input.CursorShape.CURSOR_ARROW,cursor_custom_offset)
+		_:
+			Input.set_custom_mouse_cursor(null,Input.CursorShape.CURSOR_ARROW)
